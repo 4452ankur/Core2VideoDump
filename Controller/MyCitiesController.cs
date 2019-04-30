fbcb7b28f29ccd2b1dbfdda1470c960322d051d0
4452ankur <39676548+4452ankur@users.noreply.github.com>
2019-04-30 16:31:39 +0530

    Add files via upload

---
+++ Controller/MyCitiesController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core2Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using static Microsoft.IdentityModel.Xml.WsTrustConstants_1_3;
+
+namespace Core2Api.Controller
+{
+    [Route("api/[controller]")]
+
+
+
+    [ApiController]
+    public class MyCitiesController:ControllerBase
+    {
+        //[HttpGet]
+        //public JsonResult GetAllCities()
+        //{
+        //    return new JsonResult(new List<object>
+        //    {
+        //        new{id=1,name="Kolkata"},
+        //        new{id=2,name="Kolkata2"},
+        //        new{id=3,name="Kolkata3"}
+        //    });
+        //}
+        [HttpGet]
+        [Route("GetAllCities")]
+        public JsonResult GetAllCities()
+        {
+           // return new JsonResult(CityDataStore.Current.Cities);
+           var temp= new JsonResult(CityDataStore.Current.Cities);
+            temp.StatusCode = 200;
+            return temp;
+
+
+        }
+
+        //[HttpGet("{id}")]
+        //public JsonResult GetCity(int id)
+        //{
+        //    return new JsonResult(CityDataStore.Current.Cities.FirstOrDefault(x=>x.id==id));
+        //}
+
+        [HttpGet("{id}",Name ="GetCity")]
+        public IActionResult GetCity(int id)
+        {
+            var returnCity=(CityDataStore.Current.Cities.FirstOrDefault(x => x.id == id));
+            if(returnCity==null)
+            {
+                return NotFound();
+            }
+            return Ok(returnCity);
+        }
+
+        [HttpPost]
+        public IActionResult PostCity([FromBody] City item)
+        {
+            CityDataStore.Current.Cities.Add(item);
+            return CreatedAtRoute("GetCity", new { item.id }, item);
+                     
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutCity([FromBody] City item, int id)
+        {
+            if(item==null)
+            {
+                return BadRequest();
+            }
+            if(item.Name==null & item.Description==null)
+            {
+                return BadRequest();
+            }
+            var temp = CityDataStore.Current.Cities.FirstOrDefault(x => x.id == id);
+            if(temp==null)
+            {
+                return NotFound();
+            }
+            temp.Name = item.Name;
+            temp.Description = item.Description;
+            temp.Interest = item.Interest;
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCity( int id)
+        {
+           
+            var temp = CityDataStore.Current.Cities.FirstOrDefault(x => x.id == id);
+            if (temp == null)
+            {
+                return NotFound();
+            }
+            CityDataStore.Current.Cities.Remove(temp);
+            return NoContent();
+        }
+
+    }
+}
---
+++ Models/CitySataStore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core2Api.Models
+{
+    public class CityDataStore
+    {
+        public static CityDataStore Current { get; } = new CityDataStore();
+        public List<City> Cities { get; set; }
+
+        public CityDataStore()
+        {
+            Cities = new List<City>()
+            {
+                new City{id=1,Name="Kolkata",Description="Best City"},
+                  new City{id=2,Name="Kolkata2",Description="Best City"},
+                    new City{id=3,Name="Kolkata3",Description="Best City"}
+            };
+        }
+    }
+}
